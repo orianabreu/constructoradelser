c21896aa7ae48315975986b33b8ed75e918d2974
Oriana Abreu <oriabreu.v@gmail.com>
2021-05-07 19:33:35 +0200

    set theme and fonts

---
+++ src/theme/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+    fonts: {
+        primary: 'Oswald',
+        secondary: 'Shadows Into Light',
+        text: 'Roboto',
+    },
+    scale: {
+        base: 8,
+        h1: '3.052rem',
+        h2: '2.441rem',
+        h3: '1.953rem',
+        h4: '1.563rem',
+        h5: '1.25rem',
+        paragraph: '1rem',
+        caption: '0.8rem',
+    },
+    palette: {
+        primary: '6A0893',
+        secondary: 'FA5BFA',
+        yellow: 'FEEF72',
+        light: 'F2F2FF',
+        blue: '4474FF',
+    },
+};
+
+export default theme;
